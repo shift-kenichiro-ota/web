bed2ae2d58f2fc8f3731402787f0f3687d8baa19
kenichiro.ota <kenichiro.ota@shiftinc.jp>
2014-03-11 10:46:03 +0900

    replace jquery to document

--- js/test/common.js
+++ js/test/common.js
@@ -16,10 +16,10 @@ define(function() {
 
                 //var colItem = '<li data-role="list-divider" role="heading" class="ui-li ui-li-divider ui-bar-b" id="test_case_num"' + suiteNo + '>API組み合わせ番号' + suiteNo + '</li>';
                 var colItem = '<li id="test_case_num"' + suiteNo + '>API組み合わせ番号' + suiteNo + '</li>';
-                if($("#test_case_num" + suiteNo).html() !== undefined || $("#test_case_num" + suiteNo).html() !== "") {
+                //if($("#test_case_num" + suiteNo).html() !== undefined || $("#test_case_num" + suiteNo).html() !== "") {
                     //$('#testResult').append(colItem).collapsibleset();
                     $('#testResult').append(colItem);
-                }
+                //}
                 callback(null);
             },
             function(callback) {
